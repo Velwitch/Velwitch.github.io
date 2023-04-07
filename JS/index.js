609f51fdcb3d3ad90675116d0be1a11ab9f5d929
Velwitch <phil.pynson@gmail.com>
2023-04-07 18:56:56 +0200

    darkmode done

---
+++ JS/index.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", () => {
+    console.log("The document has finished loading!");
+
+    const darkMode = document.querySelector('#darkMode');
+    const html = document.querySelector('html');
+
+    darkMode.addEventListener("click", () => {
+        console.log('qbdkqsbdbkqsj');
+        html.classList.toggle("dark");
+    });
+
+    const burgerBtn = document.querySelector("#burger");
+ 
+
+    
+});
